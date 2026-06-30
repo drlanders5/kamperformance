cd6ef2c86f3e3695662ac78dfd0f680fda07e8dd
drlanders5 <drlanders5@gmail.com>
2026-06-30 04:11:58 -0400

    Delay scroll reveal trigger

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const revealOnScroll = () => {
         const elementTop = element.getBoundingClientRect().top;
         const windowHeight = window.innerHeight;
 
-        if (elementTop < windowHeight - 220) {
+        if (elementTop < windowHeight - 350) {
             element.classList.add("active");
         }
     });
