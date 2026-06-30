8b7d5c249f14a3de2ed2d9b4ab88b442d779353f
drlanders5 <drlanders5@gmail.com>
2026-06-30 03:58:17 -0400

    Add scroll reveal script

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const revealElements = document.querySelectorAll(".reveal");
+
+const revealOnScroll = () => {
+    revealElements.forEach((element) => {
+        const elementTop = element.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+
+        if (elementTop < windowHeight - 100) {
+            element.classList.add("active");
+        }
+    });
+};
+
+window.addEventListener("scroll", revealOnScroll);
+window.addEventListener("load", revealOnScroll);
